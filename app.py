e45c9af067755fc1b7619252fd9c95fab909b72a
Robert Bost <bostrt@gmail.com>
2018-11-16 11:59:21 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import Flask, send_file, render_template
+import os
+import zipfile
+import tempfile
+import hashlib
+
+ADVERT_FILES = {
+    'ad-manifest.json',
+    'banner.gif',
+    'details.html',
+    'screensvr.png',
+    'snippet.json',
+    'thumb.gif'
+}
+datadir = os.getenv('DATA_DIR', './data')
+app = Flask(__name__)
+
+def _build_ad():
+    pass
+
+# thanks https://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+def extract_adid(input):
+    # TODO: Is there a better way to do this with
+    result = input.split('.')[0]
+    if not result:
+        return None
+    result = result.split('-')
+    if len(result) != 2:
+        return None
+    result = result[1]
+    if not result.isdigit():
+        return None
+    else:
+        return result
+
+def compress_addir(adid, timestamp, creativeid):
+    tempdir = tempfile.gettempdir()
+    context = dict()
+    ziph = zipfile.ZipFile(os.path.join(tempdir, 'advert' + adid + '.apg'), 'w', zipfile.ZIP_DEFLATED)
+
+    snippetjsonf = os.path.join(datadir, 'snippet.json')
+    ziph.write(snippetjsonf, arcname=os.path.join(adid, 'snippet.json'))
+    snippetjsonmd5 = md5(snippetjsonf)
+
+    thumbgiff = os.path.join(datadir, 'thumb.gif')
+    ziph.write(thumbgiff, arcname=os.path.join(adid, 'thumb.gif'))
+    thumbgifmd5 = md5(thumbgiff)
+
+    bannergiff = os.path.join(datadir, 'banner.gif')
+    ziph.write(bannergiff, arcname=os.path.join(adid, 'banner.gif'))
+    bannergifmd5 = md5(bannergiff)
+
+    screensvrgiff = os.path.join(datadir, 'screensvr.gif')
+    ziph.write(screensvrgiff, arcname=os.path.join(adid, 'screensvc.gif'))
+    screensvrgifmd5 = md5(screensvrgiff)
+
+    detailsxmlf = os.path.join(datadir, 'details.xml')
+    ziph.write(detailsxmlf, arcname=os.path.join(adid, 'details.xml'))
+    detailsxmlmd5 = md5(detailsxmlf)
+
+    admanifestjsonpath = os.path.join(tempdir, 'ad-manifest.json')
+    admanifestjsonf = open(admanifestjsonpath, 'w')
+    admanifestjsonf.write(render_template('ad-manifest.json.tpl', \
+        adid=adid, version=timestamp, creativeid=creativeid, \
+        screensvrgifmd5=screensvrgifmd5, thumbgifmd5=thumbgifmd5, detailsxmlmd5=detailsxmlmd5, \
+        bannergifmd5=bannergifmd5, snippetjsonmd5=snippetjsonmd5))
+    admanifestjsonf.close()
+    ziph.write(admanifestjsonpath, arcname='ad-manifest.json')
+
+    ziph.close()
+    return ziph.filename
+
+@app.route('/US/<int:a>/<int:b>/<string:adid>/<string:something>/ad-<int:adid2>.<int:timestamp>.<int:creativeid>.apg', methods=['GET'])
+def hello(a, b, adid, something, adid2, timestamp, creativeid):
+    addirzip = compress_addir(adid, timestamp, creativeid)
+    if not addirzip:
+        return 'Error building ad.'
+
+    resp = send_file(addirzip, mimetype='application/x-apg-zip')
+    resp.headers['x-amz-id-2'] = 'btOnjDPjc6CFjsCDc3EwERObf8iwIbFF7gSNijBcx4uYcvtQ+gPOFuKeTXmxXPld'
+    resp.headers['x-amz-request-id'] = '23B54F17F3191750'
+    resp.headers['Server'] = 'AmazonS3'
+    return resp
---
+++ fakecrt.sh
@@ -0,0 +1,43 @@
+openssl genrsa -out fake-adpublisher.key 2048
+openssl req -new -key fake-adpublisher.key -out fake-adpublisher.csr -subj '/C=US/ST=Washington/L=Seattle/O=Amazon.com Inc./CN=*.s3.amazonaws.com' 
+cat <<EOF > fakecsr.yaml
+apiVersion: certificates.k8s.io/v1beta1
+kind: CertificateSigningRequest
+metadata:
+  name: adpublisher-svc.cheep
+spec:
+  groups:
+  - system:authenticated
+  request: $(cat fake-adpublisher.csr | base64 | tr -d '\n')
+  usages:
+  - digital signature
+  - key encipherment
+  - server auth
+EOF
+
+# Make sure your user had csr create permission:
+#  oc create clusterrole certificate-signing-requestor --verb=get,list,create --resource=certificatesigningrequests.certificates.k8s.io
+# oc create -f fakecsr.yaml
+# Login as ocp admin and approve cert:
+#  oc adm certificates approve <name>
+# Download cert as client and use it!
+#  oc get csr adpublisher-svc.cheep -o jsonpath='{.status.certificate}'     | base64 --decode > server.crt
+# Create route
+#  oc create route edge adpublisher --cert=server.crt --key=fake-adpublisher.key --hostname=adpublisher.s3.amazonaws.com --service=go-print-request-headers 
+# Add CNAME to DNS server pointing adpublisher.s3.amazonaws.com to your app hostname or A record pointing to IP.
+# Add CA cert to /etc/ssl/certs/ca-certificates on Kindle
+
+# DEBUG
+# curl -o ad.apg -H 'Host: adpublisher.s3.amazonaws.com' -vk 'https://52.216.101.59/US/94/84/9484321190501/SHASTA/ad-9484321190501.20180409170745.1515612200601.apg?software_rev=2692310002&patchVersion=2&currentTransportMethod=WIFI&currentMCC=&currentSponsoredHotspot='
+# < HTTP/1.1 200 OK
+# < x-amz-id-2: ZRffERjiTdzelBgzto2GWtaKdOHIF2XjESK8oAIUYpf9hmZRTnq4v+E/GffCfzzIJi3s9XTXr0w=
+# < x-amz-request-id: 3544210C38446F45
+# < Date: Fri, 16 Nov 2018 17:28:42 GMT
+# < Last-Modified: Mon, 09 Apr 2018 17:07:46 GMT
+# < ETag: "05480ae8221b6c4e1c903ff19f852e43"
+# < x-amz-storage-class: REDUCED_REDUNDANCY
+# < Accept-Ranges: bytes
+# < Content-Type: application/x-apg-zip
+# < Content-Length: 311814
+# < Server: AmazonS3
+
