f7473c9feaf7d6bd55f0b73c79ea2b28692d3dfd
Robert Bost <bostrt@gmail.com>
2018-11-16 15:48:52 -0700

    Added notes and replaced screensaver.

--- fakecrt.sh
+++ fakecrt.sh
@@ -1,5 +1,5 @@
 openssl genrsa -out fake-adpublisher.key 2048
-openssl req -new -key fake-adpublisher.key -out fake-adpublisher.csr -subj '/C=US/ST=Washington/L=Seattle/O=Amazon.com Inc./CN=*.s3.amazonaws.com' 
+openssl req -new -key fake-adpublisher.key -out fake-adpublisher.csr -subj '/C=US/ST=Washington/L=Seattle/O=Amazon.com Inc./CN=*.s3.amazonaws.com'
 cat <<EOF > fakecsr.yaml
 apiVersion: certificates.k8s.io/v1beta1
 kind: CertificateSigningRequest
@@ -23,9 +23,16 @@ EOF
 # Download cert as client and use it!
 #  oc get csr adpublisher-svc.cheep -o jsonpath='{.status.certificate}'     | base64 --decode > server.crt
 # Create route
-#  oc create route edge adpublisher --cert=server.crt --key=fake-adpublisher.key --hostname=adpublisher.s3.amazonaws.com --service=go-print-request-headers 
+#  oc create route edge adpublisher --cert=server.crt --key=fake-adpublisher.key --hostname=adpublisher.s3.amazonaws.com --service=go-print-request-headers
 # Add CNAME to DNS server pointing adpublisher.s3.amazonaws.com to your app hostname or A record pointing to IP.
 # Add CA cert to /etc/ssl/certs/ca-certificates on Kindle
+
+# TODO
+# Put all these notes in a real doc
+# Other ads are also pulled from dadet-kapo-prod-flights.s3.amazonaws.com !!!
+# Example request:
+#  10.129.0.1 - - [16/Nov/2018:22:46:24 +0000] "GET /8148/8981198440101_XX-3b5a8c746b8fbd0b324e343dd802191026383609b0119db2142b27689796d4b0/bcd05f741e81540c7d5cc840dc88f66fc36aa71a55b396b98f05fe9065ed6486/1.0/ad-8981198440101_XX-3b5a8c746b8fbd0b324e343dd802191026383609b0119db2142b27689796d4b0.20180409164836.1515612200601.apg?software_rev=2692310002&patchVersion=2&currentTransportMethod=WIFI&currentMCC=&currentSponsoredHotspot= HTTP/1.1" 404 233 "-" "-"
+
 
 # DEBUG
 # curl -o ad.apg -H 'Host: adpublisher.s3.amazonaws.com' -vk 'https://52.216.101.59/US/94/84/9484321190501/SHASTA/ad-9484321190501.20180409170745.1515612200601.apg?software_rev=2692310002&patchVersion=2&currentTransportMethod=WIFI&currentMCC=&currentSponsoredHotspot='
@@ -40,4 +47,3 @@ EOF
 # < Content-Type: application/x-apg-zip
 # < Content-Length: 311814
 # < Server: AmazonS3
-
